9d03840569786491cab0c17d29d00471d742fd03
shterneregen <shterneregen@gmail.com>
2019-10-03 18:43:01 +0300

    init commit

---
+++ src/main/java/random/learning/springbootangular/model/Self.java
@@ -0,0 +1,14 @@
+package random.learning.springbootangular.model;
+
+public class Self {
+
+    private String ref;
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+}
